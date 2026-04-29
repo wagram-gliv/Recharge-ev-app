a4f562d8ad61bcf2708367cf85b05ab8634c9710
wagram-gliv <mikael.vandaele@gmail.com>
2026-04-29 22:15:49 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const APP_CONFIG = {
-  ORS_API_KEY: '', // Ajouter une clé OpenRouteService ici ou via window.__APP_CONFIG__.
+  ORS_API_KEY: 'eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6ImE4N2U2M2NjY2E1ZTRjN2M4MDUzMTAyMjc5NmNmYzBjIiwiaCI6Im11cm11cjY0In0=', // Ajouter une clé OpenRouteService ici ou via window.__APP_CONFIG__.
   ORS_BASE_URL: 'https://api.heigit.org', // Alternative Heigit: https://api.heigit.org
   CHARGER_POWER_STEPS_KW: [100, 43, 20],
   ROUTE_BUFFER_KM: 8
